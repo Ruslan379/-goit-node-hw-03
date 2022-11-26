f2e22cc76298cb007b02a84c7a4e35cdc7ff187c
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-11-27 01:02:00 +0200

    [Node.js#33] Модуль 3-6_MongoDB и Mongoose - 20

--- controllers/contacts/updatePutContact.js
+++ controllers/contacts/updatePutContact.js
@@ -1,18 +1,12 @@
+//? +++++++++++++++++++  mongoose +++++++++++++++++++
 const { NotFound } = require('http-errors')
-
-const contactsOperations = require("../../models/contacts")
-const { lineBreak } = require("../../service");
+const { Contact } = require("../../models");
 
 
 //-----------------------------------------------------------------------------
 const updatePutContact = async (req, res, next) => {
-    //! ===========================console============================
-    console.log("START-->PUT/:id".rainbow); //!
-    lineBreak();
-    //! ==============================================================
-
     const { contactId } = req.params;
-    const contact = await contactsOperations.updatePutContact(contactId, req.body)
+    const contact = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
 
     if (!contact) {
         throw new NotFound(`Contact wiht id:'${contactId}' not found`)
@@ -23,6 +17,43 @@ const updatePutContact = async (req, res, next) => {
         code: 200,
         data: { contact }
     })
-}
+};
 
-module.exports = updatePutContact
+module.exports = updatePutContact;
+//? _____________________  mongoose _____________________
+
+
+
+
+
+
+
+
+//todo --> OLD ------------------------------------
+// const { NotFound } = require('http-errors')
+// const contactsOperations = require("../../models/contacts")
+// const { lineBreak } = require("../../service");
+
+
+// //-----------------------------------------------------------------------------
+// const updatePutContact = async (req, res, next) => {
+//     //! ===========================console============================
+//     console.log("START-->PUT/:id".rainbow); //!
+//     lineBreak();
+//     //! ==============================================================
+
+//     const { contactId } = req.params;
+//     const contact = await contactsOperations.updatePutContact(contactId, req.body)
+
+//     if (!contact) {
+//         throw new NotFound(`Contact wiht id:'${contactId}' not found`)
+//     }
+
+//     res.status(200).json({
+//         status: "success",
+//         code: 200,
+//         data: { contact }
+//     })
+// }
+
+// module.exports = updatePutContact
