2873699390505c57b0dd8943332a4030618d4b5e
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-11-26 11:50:56 +0200

    [Node.js#33] Модуль 3-5_MongoDB и Mongoose - 6

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ require("colors");
 
 const contactsRouter = require('./routes/api/contacts')
 
+//? mongoose
+const mongoose = require("mongoose");
+const DB_HOST1 = "mongodb+srv://Ruslan:SiaLzikXKL7dkvK2@cluster379.kq6zkfp.mongodb.net/online_shop?retryWrites=true&w=majority";
+const DB_HOST = "mongodb+srv://Ruslan:SiaLzikXKL7dkvK2@cluster379.kq6zkfp.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose.connect(DB_HOST)
+  .then(() => console.log("Database connect"))
+  .catch(error => console.log(error.message))
 
 //----------------------------------------------------------------
 const app = express()
